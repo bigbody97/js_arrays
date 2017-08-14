70b0df464b0291cfddadfa8c8fff577b9acee958
bigbody97 <famback17@gmail.com>
2017-08-13 23:52:27 -0700

    Indexing array with more commits.

--- js/myjs.js
+++ js/myjs.js
@@ -2,5 +2,7 @@
 var numArray = [1,2,4,5,];
 
 //Indexing an array by using the variable array named colorArray.
+//When Indexing the first item in the arry has a index of [0] which will log to the console the color "red" [1] would be "black" and so on.
+
 var colorArray = ["red", "black","blue","green"];
 console.log(colorArray[0]);

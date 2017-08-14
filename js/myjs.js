987be7099e39609a2e7619d60f4c7f6d9c388e97
bigbody97 <famback17@gmail.com>
2017-08-14 01:10:05 -0700

    Array with mixed data types.

--- js/myjs.js
+++ js/myjs.js
@@ -7,8 +7,12 @@ var numArray = [1,2,4,5,];
 var colorArray = ["red", "black","blue","green"];
 console.log(colorArray[0]);
 
-// Array element reasignment: by grabing the index [1] wich is "black" and giving it a diferant value "grey".
+//Array element reasignment: by grabing the index [1] wich is "black" and giving it a diferant value "grey".
 
 var colorArray = ["red", "black","blue","green"];
 colorArray[1] = "grey";
 console.log(colorArray[1]);
+
+//Mixed data type array bool, int, string .
+var anythingGoes = [false, 100, "Big Body"];
+console.log(anythingGoes[0],anythingGoes[1],anythingGoes[2]);

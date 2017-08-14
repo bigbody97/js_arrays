2c1a12bf7854eb05758dea7a2090cf344529364c
bigbody97 <famback17@gmail.com>
2017-08-13 23:48:59 -0700

    Indexing an array.

--- js/myjs.js
+++ js/myjs.js
@@ -1,2 +1,6 @@
 //Basic numArray with 4 diferant numerical values
 var numArray = [1,2,4,5,];
+
+//Indexing an array by using the variable array named colorArray.
+var colorArray = ["red", "black","blue","green"];
+console.log(colorArray[0]);
